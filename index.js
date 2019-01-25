2b0ddb59b6a330b41b8200ba598ec8447e561d72
ImAmalox <amalox@outlook.com>
2019-01-25 18:02:44 +0100

    Evaluating bits sent by wiimote

---
+++ index.js
@@ -0,0 +1,17 @@
+const HID = require('node-hid');
+
+let VENDOR_ID = 1406;
+let PRODUCT_ID = 774;
+
+let wiimotes = HID.devices(VENDOR_ID, PRODUCT_ID);
+let paths = [];
+
+wiimotes.forEach(wiimote => {
+    paths.push(wiimote.path);
+})
+
+let wiimote = new HID.HID(paths[0]);
+
+wiimote.on('data', function(data) {
+    console.log(data);
+});
